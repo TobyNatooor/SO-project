ac0b7e1ec0a7d16d54060efa091b5d89678e42ba
tobiasnf <tobiasnf@gmail.com>
2021-02-10 13:17:04 +0100

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('navbar').innerHTML = `
+<form action="./index.html"><button>Forside</button></form>
+<form action="./teori.html"><button>Teori</button></form>
+<form action="./forsøg.html"><button>Forsøg</button></form>
+<form action="./matematik.html"><button>Matematik</button></form>
+<form action="./om-os.html"><button>Om Os</button></form>
+`
