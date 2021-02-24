23ed56041beadc745ab68655c2670ef666d369f1
tobiasnf <tobiasnf@gmail.com>
2021-02-24 20:04:23 +0100

    .

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 document.getElementById('navbar').innerHTML = `
     <form action="./index.html"><button>Forside</button></form>
     <form action="./natvidmet.html"><button>Naturvidenskabelige Metode</button></form>
-    <form action="./teori.html"><button>Teori</button></form>
     <form action="./forsøg-frem.html"><button>Forsøg Fremgangsmåde</button></form>
+    <form action="./teori.html"><button>Teori</button></form>
     <form action="./forsøg-eks.html"><button>Forsøg Eksempel</button></form>
     <form action="./beregninger.html"><button>Beregninger</button></form>
     <form action="./regn-selv.html"><button>Regn Selv</button></form>
